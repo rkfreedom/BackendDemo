8c14a0b549b65948068102851411d45698f85760
rkfreedom <rishikeshmunde@gmail.com>
2023-05-03 13:09:22 +0530

    checking

---
+++ GithubDay2/src/main/java/com/cjc/main/GithubDay2Application.java
@@ -0,0 +1,13 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDay2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubDay2Application.class, args);
+	}
+
+}
